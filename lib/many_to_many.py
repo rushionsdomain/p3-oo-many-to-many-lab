fdb573a0b43c403347e42a9edea11fa6b07ec19c
Gachuri <rushionchegge@gmail.com>
2024-09-25 12:20:13 +0300

    Added Author, Book, and Contract classes to fulfill tests

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,53 @@
-class Author:
-    pass
+class Contract:
+    all = []
 
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception("Author must be an instance of Author")
+        if not isinstance(book, Book):
+            raise Exception("Book must be an instance of Book")
+        if not isinstance(date, str):
+            raise Exception("Date must be a string")
+        if not isinstance(royalties, int):
+            raise Exception("Royalties must be an integer")
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
 
 class Book:
-    pass
+    def __init__(self, title):
+        self.title = title
+        self.contracts_list = []
 
+    def contracts(self):
+        return self.contracts_list
 
-class Contract:
-    pass
+    def authors(self):
+        return [contract.author for contract in self.contracts_list]
+
+class Author:
+    def __init__(self, name):
+        self.name = name
+        self.contracts_list = []
+
+    def contracts(self):
+        return self.contracts_list
+
+    def books(self):
+        return [contract.book for contract in self.contracts_list]
+
+    def sign_contract(self, book, date, royalties):
+        contract = Contract(self, book, date, royalties)
+        self.contracts_list.append(contract)
+        book.contracts_list.append(contract)
+        return contract
+
+    def total_royalties(self):
+        return sum(contract.royalties for contract in self.contracts_list)
